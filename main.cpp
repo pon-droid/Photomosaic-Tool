830f96389cf8e5b3de6fc6851e250f5ff19582a1
tbg <tellepsarecool@gmail.com>
2021-10-13 22:33:32 +1100

    Fixed out of array bug, implemented skeleton for color diff

--- main.cpp
+++ main.cpp
@@ -14,6 +14,8 @@ const int IMAGE_W = 50;
 const int IMAGE_H = 50;
 
 
+
+
 struct i_rgb_values {
     int r, g, b;
 };
@@ -22,8 +24,10 @@ struct i_rgb_values {
 struct s_rgb_values {
     string img_name;
     int a_r, a_g, a_b;
-
+    float dist = 0;
 };
+
+
 
 vector<i_rgb_values>  return_i_rgb(){
     Image input_image;
@@ -49,19 +53,19 @@ return input_rgb;
 }
 
 
-tuple<double, double, double> average_rgb (Image &image){
-    double aver_r = 0;
-    double aver_g = 0;
-    double aver_b = 0;
+tuple<unsigned char, unsigned char, unsigned char> average_rgb (Image &image){
+    long aver_r = 0;
+    long aver_g = 0;
+    long aver_b = 0;
 
 
     for(int y = 0; y<IMAGE_H; y++){
         for(int x = 0; x<IMAGE_W; x++){
             ColorRGB colour = image.pixelColor(x,y);
 
-            double red = colour.red() * MAX_COLOUR;
-            double green = colour.green() * MAX_COLOUR;
-            double blue = colour.blue() * MAX_COLOUR;
+            unsigned char red = colour.red() * MAX_COLOUR;
+            unsigned char green = colour.green() * MAX_COLOUR;
+            unsigned char blue = colour.blue() * MAX_COLOUR;
 
             aver_r += red;
             aver_g += green;
@@ -69,13 +73,15 @@ tuple<double, double, double> average_rgb (Image &image){
         }
     }
 
-    aver_r = aver_r/(IMAGE_W*IMAGE_H);
-    aver_g = aver_g/(IMAGE_W*IMAGE_H);
-    aver_b = aver_b/(IMAGE_W*IMAGE_H);
+
+
+    unsigned char a_red = aver_r/(IMAGE_W*IMAGE_H);
+    unsigned char a_green = aver_g/(IMAGE_W*IMAGE_H);
+    unsigned char a_blue = aver_b/(IMAGE_W*IMAGE_H);
 
 
 
-    return make_tuple(aver_r, aver_g, aver_b);
+    return make_tuple(a_red, a_green, a_blue);
 }
 
 vector<s_rgb_values> load_sources(){
@@ -95,8 +101,6 @@ vector<s_rgb_values> load_sources(){
 
         tie(a_r, a_g, a_b) = average_rgb(image_cell);
         source_rgb.push_back({entry.path(),a_r,a_g,a_b});
-        cout << source_rgb[3].a_r << endl;
-
         }
         //These below exceptions occur if the file is not an image, it will skip if the file is not an image
         catch(Magick::ErrorMissingDelegate &error){
@@ -109,17 +113,30 @@ vector<s_rgb_values> load_sources(){
     }
 
     return source_rgb;
+
 }
 
-void calc_colour_diff(vector<i_rgb_values> input_rgb, vector<s_rgb_values> source_rgb){
+int formula(int r, int g, int b, int rT, int gT, int bT){
 
+    int result = (r-rT)*(r-rT) + (g-gT)*(g-gT) + (b-bT)*(b-bT);
+
+    return result;
+}
+
+
+void calc_colour_diff(vector<i_rgb_values> input_rgb, vector<s_rgb_values> source_rgb){
+// Get list of colour difference for each pixel in the input image
+// and store it in source_rgb struct
     for(int i = 0; i<IMAGE_H*IMAGE_W; i++){
-       cout << "INPUT = " << input_rgb[i].r << " OUTPUT = " << source_rgb[i].a_r << endl;
-       cout << input_rgb[i].r - source_rgb[i].a_r << endl;
+
+        for(int j = 0; j < source_rgb.size(); j++){
+            source_rgb[j].dist = sqrt(formula(input_rgb[i].r,input_rgb[i].g,input_rgb[i].b,source_rgb[j].a_r,source_rgb[j].a_g,source_rgb[j].a_b));
+        }
 
     }
 
 }
+
 
 
 
@@ -129,10 +146,11 @@ int main(int argc,char **argv)
     InitializeMagick(*argv);
     try{
 
-
     vector<s_rgb_values> source_rgb = load_sources();
     vector<i_rgb_values> input_rgb = return_i_rgb();
+
     calc_colour_diff(input_rgb, source_rgb);
+
 
     }
     catch( Exception &error_ )
